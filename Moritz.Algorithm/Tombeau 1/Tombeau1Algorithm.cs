68e6256f30ee726f433dabdc3676226f8db7223a
notator <j.ingram@netcologne.de>
2016-03-30 12:06:33 +0200

    Edited main comment in Tombeau1Algorithm.cs

--- Moritz.Algorithm/Tombeau 1/Tombeau1Algorithm.cs
+++ Moritz.Algorithm/Tombeau 1/Tombeau1Algorithm.cs
@@ -61,15 +61,80 @@ namespace Moritz.Algorithm.Tombeau1
 			> how notes/chords relate vertically inside a seq (How trks relate to each other inside seqs?),
 			> and about how seqs relate to each other globally...
 
-			Seqs can be superimposed, juxtaposed, repeated and re-ordered.
+			Seqs can be superimposed, juxtaposed, repeated (with/without variation) and re-ordered.
 			
 			Chords:
-			1. "Chords are colour" (Stockhausen)
-			2. Boulez' chord addition has been implemented in the function MidiChordDef.AddNotes(MidiChordDef).
-			3. Chord pitch transposition has already been implemented in the function MidiChordDef.Transpose(MidiChordDef),
-			   but it contains known bugs. Chord velocity transposition should be implemented analogously.
-			4. Chords can have holes... Do I need filters? Maybe a MidiChordDef.SubtractNotes(...) function? 
-			5. Velocity gradients in the root chords of additions: bottom->top (="consonant") --> top->bottom (="dissonant")...
+			** "Chords are colour" (Stockhausen)
+            ** I want to keep the number of pitches in chords fairly low, so that they are recognisable. Strategies like
+               Boulez' chord addition don't really work...
+            ** Chords can have any pitches (construct some interesting palettes with some differently coloured chords).
+               But velocities can be controlled independently to emphasise "harmonic" relations in areas of the score.
+               Repeated chord sequences can be thus "filtered" differently as in a digital painting...
+            ** Write a function that sets note velocities according to the position of the note's pitch in a (harmonic)
+               hierarchy of *absolute pitches* (not intervals):
+               The absolute, equal temperament pitch hierarchy relating to the harmonic series can be found as follows
+               (decimals rounded to 3 figures):
+                 absolute    equal              harmonic:     absolute         closest
+                 pitch:   temperament                         harmonic     equal temperament
+                            factor:                            factor:      absolute pitch:
+                    0:       1.000       |          2   ->   2/2  = 1.000  ->     0:
+                    1:       1.059       |          3   ->   3/2  = 1.500  ->     7:
+                    2:       1.122       |          5   ->   5/4  = 1.250  ->     4:
+                    3:       1.189       |          7   ->   7/4  = 1.750  ->     10:
+                    4:       1.260       |          9   ->   9/8  = 1.125  ->     2:
+                    5:       1.335       |         11   ->  11/8  = 1.375  ->     5:
+                    6:       1.414       |         13   ->  13/8  = 1.625  ->     9:
+                    7:       1.498       |         15   ->  15/8  = 1.875  ->     11:
+                    8:       1.587       |         17   ->  17/16 = 1.063  ->     1:
+                    9:       1.682       |         19   ->  19/16 = 1.187  ->     3:
+                    10:      1.782       |         21   ->  21/16 = 1.313  ->     
+                    11:      1.888       |         23   ->  23/16 = 1.438  ->     6:
+                                         |         25   ->  25/16 = 1.563  ->     8:
+
+               Giving the absolute pitch hierarchy:
+                    0, 7, 4, 10, 2, 5, 9, 11, 1, 3, 6, 8         
+               
+               Octaves are treated equivalently:
+                    12, 24, 36 etc. will be given the same velocity as the base pitch 0,
+                    19, 31, 43 etc. will be given the same velocity as the fifth (7),
+                    etc.
+
+               The pitchHierarchy could be chosen from a linear or circular field that includes the above absolute hierarchy.
+               I think I should try both linear and circular pitch hierarchy tables, to see which is more effective.
+
+               The pitches are all relative to 0, so the 0 should probably be omitted.
+
+               The function could be defined as:
+                        MidiChordDef.SetVelocities( int baseAbsPitch, // The (absolute) base pitch for the pitch hierarchy.
+                                                    int baseVelocity, // the velocity given to any absolute base pitch (if it exists) in the MidiChordDef
+                                                    List<int> pitchHierarchy, // e.g. [7, 4, 10, 2, 5, 9, 11, 1, 3, 6, 8] or [8, 6, 3, 1, 11, 9, 5, 2, 10, 4, 7] etc.
+                                                    List<int> velocityHierarchy, // velocities... (see below).
+                                                  );
+               The velocityHierarchy is the same length as the pitchHierarchy, and contains a set of values that are always
+               in descending order. The values are factors by which to multiply the baseVelocity for each of the correspondng
+               pitches in the pitchHierarchy. The factors are calculated (like eccentricity) using a radius coordinate at 0 degrees
+               in a semicircular linear field: the percentages being calculated from the (squares of the) distances to the foci.
+               Maybe hide the calculation of the factors inside the function, and just pass a velocityEccentricity value argument
+               (in the range [1..7])
+               The pitch hierarchy table could also be hidden...
+               The function would then reduce to:
+                        MidiChordDef.SetVelocities( int baseAbsPitch, // The (absolute) base pitch for the pitch hierarchy.
+                                                    int baseVelocity, // the velocity given to any absolute base pitch (if it exists) in the MidiChordDef
+                                                    int pitchHierarchyIndex,
+                                                    int velocityEccentricity
+                                                  );
+               Both the pitchHierarchy and velocityHierarchy tables would then be static objects inside the function.
+               Interesting that this function has density, ecentricity and absolute hierarchy inputs! (The density is the density of
+               the MidiChordDef.)
+                
+
+            ** Move unused VoiceDef and Trk functions into "unused" or "Song Six" #region, or into separate "partial class" files.
+             
+			//2. Boulez' chord addition has been implemented in the function MidiChordDef.AddNotes(MidiChordDef).
+			//3. Chord pitch transposition has already been implemented in the function MidiChordDef.Transpose(MidiChordDef),
+			//   but it contains known bugs. Chord velocity transposition could be implemented analogously.
+			//4. Chords can have holes... Do I need filters? Maybe a MidiChordDef.SubtractNotes(...) function? 
+			//5. Velocity gradients in the root chords of additions: bottom->top (="consonant") --> top->bottom (="dissonant")...
 			
 			*********************************************************************************************/
             #endregion main comments
