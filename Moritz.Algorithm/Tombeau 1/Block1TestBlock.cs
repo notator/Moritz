411e3b6f0e6388744b946b2f58e5bf4f31fd9953
notator <j.ingram@netcologne.de>
2016-07-06 12:24:53 +0200

    Working on Tombeau1's first Block
    Added a new trk.

--- Moritz.Algorithm/Tombeau 1/Block1TestBlock.cs
+++ Moritz.Algorithm/Tombeau 1/Block1TestBlock.cs
@@ -21,27 +21,38 @@ namespace Moritz.Algorithm.Tombeau1
 
         private Block GetBars1and2FromTTTrks(List<List<Trk>> TTTrks)
         {
-            Trk channel0Trk = GetChannelTrk(0, TTTrks[0][0]); // absolutePitchHierarchy(0, 0); NPitchesPerOctave = 9
-            channel0Trk.AdjustVelocitiesHairpin(0, channel0Trk.EndMsPositionReFirstIUD, 0.1, 1);
+            int midiChannel = 0;
+            Trk trk0 = GetChannelTrk(midiChannel++, TTTrks[0][0]); // absolutePitchHierarchy(0, 0); NPitchesPerOctave = 9
+            trk0.AdjustVelocitiesHairpin(0, trk0.EndMsPositionReFirstIUD, 0.1, 1);
 
-            Gamut gamut = ((MidiChordDef)channel0Trk[0]).Gamut;
+            Gamut gamut = ((MidiChordDef)trk0[0]).Gamut;
 
-            Trk channel1Trk = GetChannelTrk(1, TTTrks[0][1]);
-            channel1Trk.AdjustVelocitiesHairpin(0, channel1Trk.EndMsPositionReFirstIUD, 1, 0.1);
-            List<byte> velocityPerAbsolutePitch = ((MidiChordDef)channel1Trk[0]).Gamut.GetVelocityPerAbsolutePitch(5, false);
-            channel1Trk.SetVelocityPerAbsolutePitch(velocityPerAbsolutePitch);
-            channel1Trk.TransposeInGamut(-(gamut.NPitchesPerOctave));
-            channel1Trk.MsPositionReContainer = 231;
+            int initialDelay = 1500;
+            Trk trk1 = trk0.Clone();
+            trk1.MidiChannel = midiChannel++;
+            trk1.TransposeInGamut(8);
+            trk1.MsPositionReContainer = initialDelay;
+            trk1.MsDuration = trk0.MsDuration - (initialDelay / 2); 
+
+            Trk trk2 = GetChannelTrk(midiChannel++, TTTrks[0][1]);
+            trk2.AdjustVelocitiesHairpin(0, trk2.EndMsPositionReFirstIUD, 1, 0.1);
+            List<byte> velocityPerAbsolutePitch = ((MidiChordDef)trk2[0]).Gamut.GetVelocityPerAbsolutePitch(5, false);
+            trk2.SetVelocityPerAbsolutePitch(velocityPerAbsolutePitch);
+            trk2.TransposeInGamut(-(gamut.NPitchesPerOctave));
+            trk2.MsPositionReContainer = 231;
 
             List<Trk> trks = new List<Trk>();
-            trks.Add(channel0Trk);
-            trks.Add(channel1Trk);
+            trks.Add(trk0);
+            trks.Add(trk1);
+            trks.Add(trk2);
             Seq seq0 = new Seq(0, trks, MidiChannelIndexPerOutputVoice);
-            int barline1MsPosition = channel0Trk.MsDuration;
+            int barline1MsPosition = trk0.MsDuration;
 
             Seq seq1 = seq0.Clone();
-            seq1.Trks[1].TransposeInGamut(-((int)(gamut.NPitchesPerOctave * 1.5)));
-            seq1.Trks[1].InsertClefChange(0, "b");
+            seq1.Trks[1].MsPositionReContainer = (initialDelay / 2);
+            //seq1.Trks[2].TransposeInGamut(-((int)(gamut.NPitchesPerOctave * 1.5)));
+            seq1.Trks[2].TransposeInGamut(-2);
+            //seq1.Trks[2].InsertClefChange(0, "b");
 
             seq0.Concat(seq1);
             int barline2MsPosition = seq0.MsDuration;
