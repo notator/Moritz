1a30a26efb63dc454459c0dd53acdc2b049e4d62
notator <j.ingram@netcologne.de>
2019-12-05 16:32:27 +0100

    Study 4: Making progress with palettes and FitToGamut()...
    This commit compiles and runs. FitToGamut is not complete, but considerabla progress is being made.

--- Moritz.Algorithm/Study 4/GetTrksAndBarlines.cs
+++ Moritz.Algorithm/Study 4/GetTrksAndBarlines.cs
@@ -82,13 +82,21 @@ namespace Moritz.Algorithm.Study4
 
 			List<Trk> ch0RegionTrks = null;
 			var rSBI = RegionStartBarIndices;
-			List<int> nBarsPerRegion = new List<int>();
-			for(int i = 0; i < rSBI.Count - 1; i++)
-			{
-				ch0RegionTrks = GetCh0RegionTrks(i, gamuts, rSBI[i], rSBI[i+1]);
-				topTrks.AddRange(ch0RegionTrks);
-			}
-			ch0RegionTrks = GetCh0RegionTrks(rSBI.Count - 1, gamuts, rSBI[rSBI.Count - 1], gamuts.Count);
+
+			ch0RegionTrks = GetCh0_Region0_Trks(gamuts, rSBI[0], rSBI[1]);
+			topTrks.AddRange(ch0RegionTrks);
+
+			ch0RegionTrks = GetCh0RegionTrks(1, gamuts, rSBI[1], rSBI[2]);
+			topTrks.AddRange(ch0RegionTrks);
+			ch0RegionTrks = GetCh0RegionTrks(2, gamuts, rSBI[2], rSBI[3]);
+			topTrks.AddRange(ch0RegionTrks);
+			ch0RegionTrks = GetCh0RegionTrks(3, gamuts, rSBI[3], rSBI[4]);
+			topTrks.AddRange(ch0RegionTrks);
+			ch0RegionTrks = GetCh0RegionTrks(4, gamuts, rSBI[4], rSBI[5]);
+			topTrks.AddRange(ch0RegionTrks);
+			ch0RegionTrks = GetCh0RegionTrks(5, gamuts, rSBI[5], rSBI[6]);
+			topTrks.AddRange(ch0RegionTrks);
+			ch0RegionTrks = GetCh0RegionTrks(6, gamuts, rSBI[6], gamuts.Count);
 			topTrks.AddRange(ch0RegionTrks);
 
 			int msPos = 0;
@@ -102,69 +110,109 @@ namespace Moritz.Algorithm.Study4
 		}
 
 		// endIndex is non-inclusive
-		private List<Trk> GetCh0RegionTrks(int regionIndex, IReadOnlyList<Gamut> gamuts, int startGamutIndex, int endGamutIndex)
+		private List<Trk> GetCh0_Region0_Trks(IReadOnlyList<Gamut> gamuts, int startGamutIndex, int endGamutIndex)
 		{
 			List<Trk> regionTrks = new List<Trk>(); // return
-			switch(regionIndex)
+
+			Palette palette = _palettes[0];
+			int iudIndex = 0;
+			int minBarMsDuration = 6000;
+
+			for(int i = startGamutIndex; i < endGamutIndex; i++)
 			{
-				case 0:
-					Palette palette = _palettes[0];
-					int iudIndex = 0;
-					int minBarMsDuration = 5000;
-
-					for(int i = startGamutIndex; i < endGamutIndex; i++)
+				Trk barTrk = new Trk(0);
+				regionTrks.Add(barTrk); // region 0 only has one bar
+				var barMsDuration = 0;
+				while(barMsDuration <= minBarMsDuration)
+				{
+					var iud = palette.GetIUniqueDef((iudIndex++) % palette.Count);
+					if(iud is MidiChordDef mcd)
 					{
-						Trk barTrk = new Trk(0);
-						regionTrks.Add(barTrk); // region 0 only has one bar
-						var barMsDuration = 0;
-						while(barMsDuration <= minBarMsDuration)
-						{
-							var iud = palette.GetIUniqueDef((iudIndex++) % palette.Count);
-							if(iud is MidiRestDef)
-							{
-								iud = palette.GetIUniqueDef(0);
-							}
-							iud = FitToGamut(gamuts[i], iud);
+						mcd = FitToGamut(gamuts[i], mcd, 0, 0);
 
-							barMsDuration += iud.MsDuration;
+						barMsDuration += mcd.MsDuration;
 
-							barTrk.Add(iud);
-						}
+						barTrk.Add(mcd);
 					}
-					break;
-				default:
-					Palette defaultPalette = _palettes[0];
-					iudIndex = 0;
-					minBarMsDuration = 1000;
-
-					for(int i = startGamutIndex; i < endGamutIndex; i++)
-					{
-						Trk barTrk = new Trk(0);
-						regionTrks.Add(barTrk);
-						var barMsDuration = 0;
-						while(barMsDuration <= minBarMsDuration)
-						{
-							var iud = defaultPalette.GetIUniqueDef((iudIndex++) % defaultPalette.Count);
-							if(iud is MidiRestDef)
-							{
-								iud = defaultPalette.GetIUniqueDef(0);
-							}
-							iud = FitToGamut(gamuts[i], iud);
-
-							barMsDuration += iud.MsDuration;
-
-							barTrk.Add(iud);
-						}
-					}
-					break;
-
+				}
 			}
+
 			return regionTrks;
 		}
 
-		private IUniqueDef FitToGamut(Gamut gamut, IUniqueDef iud)
+		// endIndex is non-inclusive
+		private List<Trk> GetCh0RegionTrks(int regionIndex, IReadOnlyList<Gamut> gamuts, int startGamutIndex, int endGamutIndex)
 		{
-			return iud.Clone() as IUniqueDef;
+			List<Trk> regionTrks = new List<Trk>(); // return
+			Palette defaultPalette = _palettes[0];
+			int iudIndex = 0;
+			int minBarMsDuration = 1000;
+
+			for(int i = startGamutIndex; i < endGamutIndex; i++)
+			{
+				Trk barTrk = new Trk(0);
+				regionTrks.Add(barTrk);
+				var barMsDuration = 0;
+				while(barMsDuration <= minBarMsDuration)
+				{
+					var iud = defaultPalette.GetIUniqueDef((iudIndex++) % defaultPalette.Count);
+					if(iud is MidiChordDef mcd)
+					{
+						mcd = FitToGamut(gamuts[i], mcd, 0, 0);
+
+						barMsDuration += mcd.MsDuration;
+
+						barTrk.Add(mcd);
+					}
+				}
+			}
+
+			return regionTrks;
+		}
+
+		/// <summary>
+		/// The argument MidiChordDef determines the following parameters in the returned MidiChordDef:
+		/// (These should be set using a palette.)
+		///    1. the number of BasicMidiChordDefs and their pitch density
+		///    2. the root pitch will be as high as possible, but less than or equal to BasicMidiChordDef[0].Pitches[0]. 
+		///    2. pitchWheelDeviation
+		///    3. pitchWheel and pan slider definitions
+		/// ------------------------------------------------------------------
+		/// The root (lowest) pitch in the returned MidiChordDef is found as follows:
+		///    1. The root's absolute pitch is found using absRootIndex in the gamut.AbsolutePitches list.
+		///    2. The root's relative pitch is the highest root pitch less than or equal to the lowest pitch in the argument MidiChordDef.
+		/// The shape of the output chord is determined by the chordShapeIndex using a static set of Study4ChordShapes as follows:
+		///    1. Find the chordShape in Study4ChordShapes using the chordShapeIndex.
+		///       A Study4ChordShape is a list of 7 indices for use in the gamut.AbsolutePitches list.
+		///       These indices may repeat in the list (resulting in octaves).
+		///    2. Find the corresponding list of absolute pitches using only the number of pitches defined by the MidiChordDef argument.
+		///    3. Build the chord upwards from the root, copying the velocities that correspond to the pitches from the gamut.    
+		/// </summary>
+		private MidiChordDef FitToGamut(Gamut gamut, MidiChordDef mcd, int absRootIndex, int chordShapeIndex)
+		{
+			List<IUniqueDef> basicChords = new List<IUniqueDef>();
+
+			foreach(var bmc in mcd.BasicMidiChordDefs)
+			{
+				List<byte> pitches = new List<byte>();
+				List<byte> velocities = new List<byte>();
+				for(int i = 0; i < bmc.Pitches.Count; i++)
+				{ 
+					// Do the pitch and velocity changes here.
+
+					pitches.Add((byte)(bmc.Pitches[i] - 12));
+					velocities.Add(bmc.Velocities[i]);
+				}
+				basicChords.Add(new MidiChordDef(pitches, velocities, mcd.MsDuration, mcd.HasChordOff));
+			}
+
+			var rval = new MidiChordDef(mcd.MsDuration, basicChords, mcd.OrnamentText)
+			{
+				PitchWheelDeviation = mcd.PitchWheelDeviation,
+				MidiChordSliderDefs = mcd.MidiChordSliderDefs
+			};
+
+			return rval;
 		}
 	}
 }
