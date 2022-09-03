56bfb6a1d91d57fc608ca3693b93fa4a05ed5081
notator <j.ingram@netcologne.de>
2022-09-03 12:54:43 +0200

    KrystalFamily Dependencies list is now correct...
    It is now loaded recursively, so that dependencies of dependencies are also loaded.

--- Krystals4ObjectLibrary/KrystalFamily.cs
+++ Krystals4ObjectLibrary/KrystalFamily.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Drawing;
 using System.IO;
 using System.Text;
@@ -13,12 +15,6 @@ namespace Krystals4ObjectLibrary
     ///     Input1: density input krystal
     ///     Input2: points input krystal
     ///     Field: the name of the expander
-    /// ShapedExpansionKrystal:
-    ///     Input1: density input krystal
-    ///     Input2: points input krystal
-    ///     Input3: axis input krystal
-    ///     Input4: contour input krystal
-    ///     Field: the name of the expander
     /// ModulationKrystal:
     ///     Input1: X input krystal
     ///     Input2: Y input krystal
@@ -27,7 +23,10 @@ namespace Krystals4ObjectLibrary
     ///     Input1: the krystal to be permuted
     ///     Input2: axis input krystal
     ///     Input3: contour input krystal
-    ///     (The level and sortFirst parameters are not represented here because they are not krystals.)
+    ///     (The level and sortFirst parameters are not represented here.)
+    /// PathKrystal
+    ///     Input1 = density input krystal;
+    ///     Input2 = SVG input;
     /// </summary>
     public class Dependency
     {
@@ -74,123 +73,25 @@ namespace Krystals4ObjectLibrary
 				_dependencyList.Add(d);
             }
 
-            #region add expansions to the _unknownParentsList
-            string expansions = "*.exp.krys";
-            fileInfos = dir.GetFiles(expansions);
-            foreach(FileInfo f in fileInfos)
+            List<Dependency> unknownParentsList = new List<Dependency>();
+            string krystalNames = "*.krys";
+            fileInfos = dir.GetFiles(krystalNames);
+            foreach(FileInfo fileInfo in fileInfos)
             {
-                string path = K.KrystalsFolder + @"\" +  f.Name;
-                var xk = new ExpansionKrystal(path, true);
-                Dependency d = new Dependency
+                if(krystalsUsedInScores.Contains(fileInfo.Name))
                 {
-                    Name = f.Name
-				};
-                if(xk != null)
-                {
-                    d.Input1 = xk.DensityInputFilename;
-                    d.Input2 = xk.PointsInputFilename;
-                    d.Field = xk.ExpanderFilename;
-                }
-                if(krystalsUsedInScores.Contains(d.Name))
-                {
-                    SetUsedInScoreColor(d.Name);
-                    SetAncestorColor(d.Input1);
-                    SetAncestorColor(d.Input2);
-                    SetAncestorColor(d.Field);
-                }
-                _unknownParentsList.Add(d);
+                    AddAllDependenciesToUnknownParentsList(unknownParentsList, krystalsUsedInScores, fileInfos, fileInfo);
+                }                
             }
-            #endregion add expansions to the _unknownParentsList
-
-            #region add modulations to the _unknownParentsList
-            string allModulations = "*.mod.krys";
-            fileInfos = dir.GetFiles(allModulations);
-            foreach(FileInfo f in fileInfos)
-            {
-                string path = K.KrystalsFolder + @"\" + f.Name;
-                var mk = new ModulationKrystal(path, true);
-				Dependency d = new Dependency
-				{
-					Name = f.Name
-				};
-                if(mk != null)
-                {
-                    d.Input1 = mk.XInputFilename;
-                    d.Input2 = mk.YInputFilename;
-                    d.Field = mk.ModulatorFilename;
-                }
-                if(krystalsUsedInScores.Contains(d.Name))
-                {
-                    SetUsedInScoreColor(d.Name);
-                    SetAncestorColor(d.Input1);
-                    SetAncestorColor(d.Input2);
-                    SetAncestorColor(d.Field);
-                }
-                _unknownParentsList.Add(d);
-            }
-            #endregion add modulations to the _unknownParentsList
-
-            #region add permutation krystals to the _unknownParentsList
-            string allPermutations = "*.perm.krys";
-            fileInfos = dir.GetFiles(allPermutations);
-            foreach(FileInfo f in fileInfos)
-            {
-                string path = K.KrystalsFolder + @"\" + f.Name;
-                var pk = new PermutationKrystal(path, true);
-                Dependency d = new Dependency
-                {
-                    Name = f.Name
-                };
-                if(pk != null)
-                {
-                    d.Input1 = pk.SourceInputFilename;
-                    d.Input2 = pk.AxisInputFilename;
-                    d.Input3 = pk.ContourInputFilename;
-                }
-                if(krystalsUsedInScores.Contains(d.Name))
-                {
-                    SetUsedInScoreColor(d.Name);
-                    SetAncestorColor(d.Input1);
-                    SetAncestorColor(d.Input2);
-                    SetAncestorColor(d.Input3);
-                }
-                _unknownParentsList.Add(d);
-            }
-            #endregion add path krystals to the _unknownParentsList
-
-            #region add path krystals to the _unknownParentsList
-            string allPaths = "*.path.krys";
-            fileInfos = dir.GetFiles(allPaths);
-            foreach(FileInfo f in fileInfos)
-            {
-                string path = K.KrystalsFolder + @"\" + f.Name;
-                var pk = new PathKrystal(path, true);
-                Dependency d = new Dependency
-                {
-                    Name = f.Name
-                };                
-                if(pk != null)
-                {
-                    d.Input1 = pk.DensityInputKrystalName;
-                    d.Input2 = pk.SVGInputFilename;
-                }
-                if(krystalsUsedInScores.Contains(d.Name))
-                {
-                    SetUsedInScoreColor(d.Name);
-                    SetAncestorColor(d.Input1);
-                    SetAncestorColor(d.Input2);
-                }
-                _unknownParentsList.Add(d);
-            }
-            #endregion add path krystals to the _unknownParentsList
 
             #region insert Dependencies from the _unknownParentsList in the sorted _dependencyList
 
             // The _dependency list is going to be in order of dependence. In other words:
             // Each dependency object's _inputs_ (if it has any) will have an index
             // less than the index of the dependency object itself.
+
             int[] inputIndex = new int[4];
-            foreach(Dependency d in _unknownParentsList)
+            foreach(Dependency d in unknownParentsList)
             {
                 inputIndex[0] = inputIndex[1] = inputIndex[2] = inputIndex[3] = -1;
                 if(string.IsNullOrEmpty(d.Input1) == false)
@@ -237,6 +138,143 @@ namespace Krystals4ObjectLibrary
             #endregion move Dependencies from the _unknownParentsList to the sorted _dependencyList
         }
 
+        private void AddAllDependenciesToUnknownParentsList(List<Dependency> unknownParentsList, List<string> krystalsUsedInScores, FileInfo[] fileInfos, FileInfo fileInfo)
+        {
+            if(unknownParentsList.Find(x => x.Name == fileInfo.Name) == null)
+            {
+                Dependency d = null;
+                if(krystalsUsedInScores.Contains(fileInfo.Name))
+                {
+                    d = new Dependency
+                    {
+                        Name = fileInfo.Name
+                    };
+                    SetUsedInScoreColor(d.Name);
+                }
+
+                if(fileInfo.Name.Contains(".exp."))
+                {
+                    AddExpansionDependency(d, unknownParentsList, krystalsUsedInScores, fileInfos, fileInfo);
+                }
+                else if(fileInfo.Name.Contains(".mod."))
+                {
+                    AddModulationDependency(d, unknownParentsList, krystalsUsedInScores, fileInfos, fileInfo);
+                }
+                else if(fileInfo.Name.Contains(".perm."))
+                {
+                    AddPermutationDependency(d, unknownParentsList, krystalsUsedInScores, fileInfos, fileInfo);
+                }
+                else if(fileInfo.Name.Contains(".perm."))
+                {
+                    AddPathDependency(d, unknownParentsList, krystalsUsedInScores, fileInfos, fileInfo);
+                }
+            }
+        }
+
+        private void AddExpansionDependency(Dependency d, List<Dependency> unknownParentsList, List<string> krystalsUsedInScores, FileInfo[] fileInfos, FileInfo fileInfo)
+        {
+            Debug.Assert(unknownParentsList.Find(x => x.Name == fileInfo.Name) == null);
+
+            string path = K.KrystalsFolder + @"\" + fileInfo.Name;
+            var xk = new ExpansionKrystal(path, true);
+            if(d == null)
+            {
+                d = new Dependency() { Name = fileInfo.Name };
+            }
+            d.Input1 = xk.DensityInputFilename;
+            d.Input2 = xk.PointsInputFilename;
+            d.Field = xk.ExpanderFilename;
+
+            var fileInfo1 = Array.Find(fileInfos, x => x.Name == d.Input1);
+            AddAllDependenciesToUnknownParentsList(unknownParentsList, krystalsUsedInScores, fileInfos, fileInfo1);
+            var fileInfo2 = Array.Find(fileInfos, x => x.Name == d.Input2);
+            AddAllDependenciesToUnknownParentsList(unknownParentsList, krystalsUsedInScores, fileInfos, fileInfo2);
+
+            SetAncestorColor(d.Name);
+            SetAncestorColor(d.Input1);
+            SetAncestorColor(d.Input2);
+            SetAncestorColor(d.Field);
+            
+            unknownParentsList.Add(d);
+        }
+
+        private void AddModulationDependency(Dependency d, List<Dependency> unknownParentsList, List<string> krystalsUsedInScores, FileInfo[] fileInfos, FileInfo fileInfo)
+        {
+            Debug.Assert(unknownParentsList.Find(x => x.Name == fileInfo.Name) == null);
+
+            string path = K.KrystalsFolder + @"\" + fileInfo.Name;
+            var mk = new ModulationKrystal(path, true);
+            if(d == null)
+            {
+                d = new Dependency() { Name = fileInfo.Name };
+            }
+            d.Input1 = mk.XInputFilename;
+            d.Input2 = mk.YInputFilename;
+            d.Field = mk.ModulatorFilename;
+
+            var fileInfo1 = Array.Find(fileInfos, x => x.Name == d.Input1);
+            AddAllDependenciesToUnknownParentsList(unknownParentsList, krystalsUsedInScores, fileInfos, fileInfo1);
+            var fileInfo2 = Array.Find(fileInfos, x => x.Name == d.Input2);
+            AddAllDependenciesToUnknownParentsList(unknownParentsList, krystalsUsedInScores, fileInfos, fileInfo2);
+
+            SetAncestorColor(d.Name);
+            SetAncestorColor(d.Input1);
+            SetAncestorColor(d.Input2);
+            SetAncestorColor(d.Field);
+            
+            unknownParentsList.Add(d);
+        }
+
+        private void AddPermutationDependency(Dependency d, List<Dependency> unknownParentsList, List<string> krystalsUsedInScores, FileInfo[] fileInfos, FileInfo fileInfo)
+        {
+            Debug.Assert(unknownParentsList.Find(x => x.Name == fileInfo.Name) == null);
+
+            string path = K.KrystalsFolder + @"\" + fileInfo.Name;
+            var pk = new PermutationKrystal(path, true);
+            if(d == null)
+            {
+                d = new Dependency() { Name = fileInfo.Name };
+            }
+            d.Input1 = pk.SourceInputFilename;
+            d.Input2 = pk.AxisInputFilename;
+            d.Input3 = pk.ContourInputFilename;
+
+            var fileInfo1 = Array.Find(fileInfos, x => x.Name == d.Input1);
+            AddAllDependenciesToUnknownParentsList(unknownParentsList, krystalsUsedInScores, fileInfos, fileInfo1);
+            var fileInfo2 = Array.Find(fileInfos, x => x.Name == d.Input2);
+            AddAllDependenciesToUnknownParentsList(unknownParentsList, krystalsUsedInScores, fileInfos, fileInfo2);
+            var fileInfo3 = Array.Find(fileInfos, x => x.Name == d.Input3);
+            AddAllDependenciesToUnknownParentsList(unknownParentsList, krystalsUsedInScores, fileInfos, fileInfo3);
+
+            SetAncestorColor(d.Name);
+            SetAncestorColor(d.Input1);
+            SetAncestorColor(d.Input2);
+            SetAncestorColor(d.Input3);
+
+            unknownParentsList.Add(d);
+        }
+        private void AddPathDependency(Dependency d, List<Dependency> unknownParentsList, List<string> krystalsUsedInScores, FileInfo[] fileInfos, FileInfo fileInfo)
+        {
+            Debug.Assert(unknownParentsList.Find(x => x.Name == fileInfo.Name) == null);
+
+            string path = K.KrystalsFolder + @"\" + fileInfo.Name;
+            var pk = new PathKrystal(path, true);
+            if(d == null)
+            {
+                d = new Dependency() { Name = fileInfo.Name };
+            }
+            d.Input1 = pk.DensityInputKrystalName;
+            d.Input2 = pk.SVGInputFilename;
+
+            var fileInfo1 = Array.Find(fileInfos, x => x.Name == d.Input1);
+            AddAllDependenciesToUnknownParentsList(unknownParentsList, krystalsUsedInScores, fileInfos, fileInfo1);
+
+            SetAncestorColor(d.Name);
+            SetAncestorColor(d.Input1);
+
+            unknownParentsList.Add(d);
+        }
+
         private void SetAncestorColor(string name)
         {
             if(NameColors.ContainsKey(name) && NameColors[name] == UsedInScoreColor)
@@ -247,21 +285,17 @@ namespace Krystals4ObjectLibrary
             {
                 this.NameColors.Add(name, AncestorColor);
             }
-            else
-            {
-                NameColors[name] = AncestorColor;
-            }
         }
 
         private void SetUsedInScoreColor(string name)
         {
             if(!NameColors.ContainsKey(name))
             {
-                this.NameColors.Add(name, Color.Red);
+                this.NameColors.Add(name, UsedInScoreColor);
             }
             else
             {
-                NameColors[name] = Color.Red;
+                NameColors[name] = UsedInScoreColor;
             }
         }
 
@@ -279,51 +313,57 @@ namespace Krystals4ObjectLibrary
 
         public void Rebuild()
         {
-            if(_unknownParentsList.Count > 0)
-            {
-                StringBuilder orphans = new StringBuilder();
-                foreach(Dependency d in _unknownParentsList)
-                {
-                    orphans.Append("\n");
-                    orphans.Append(d.Name);
-                }
-                string msg =
-					"The following krystals contain fatal errors,\n" +
-					"which mean that they cannot be (re)constructed:\n" + orphans.ToString() +
-					"\n\nDelete?";
-                DialogResult result = MessageBox.Show(msg, "Error", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation);
-                if(result == DialogResult.Yes)
-                    foreach(Dependency d in _unknownParentsList)
-                        File.Delete(K.KrystalsFolder + @"\" +  d.Name);
-            }
+            throw new NotImplementedException("TODO");
 
-            foreach(Dependency d in _dependencyList)
-            {
-                string path = K.KrystalsFolder + @"\" +  d.Name;
-                if(K.IsExpansionKrystalFilename(d.Name))
-                {
-                    ExpansionKrystal xk = new ExpansionKrystal(path, true);
-                }
-                if(K.IsModulationKrystalFilename(d.Name))
-                {
-                    ModulationKrystal mk = new ModulationKrystal(path, true);
-                }
-                if(K.IsPermutationKrystalFilename(d.Name))
-                {
-                    PermutationKrystal pk = new PermutationKrystal(path, true);
-                }
-                if(K.IsPathKrystalFilename(d.Name))
-                {
-                    PathKrystal pk = new PathKrystal(path, true);
-                }
-            }
+            // old code
+            //if(_unknownParentsList.Count > 0)
+            //{
+            //    StringBuilder orphans = new StringBuilder();
+            //    foreach(Dependency d in _unknownParentsList)
+            //    {
+            //        orphans.Append("\n");
+            //        orphans.Append(d.Name);
+            //    }
+            //    string msg =
+            //        "The following krystals contain fatal errors,\n" +
+            //        "which mean that they cannot be (re)constructed:\n" + orphans.ToString() +
+            //        "\n\nDelete?";
+            //    DialogResult result = MessageBox.Show(msg, "Error", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation);
+            //    if(result == DialogResult.Yes)
+            //        foreach(Dependency d in _unknownParentsList)
+            //            File.Delete(K.KrystalsFolder + @"\" + d.Name);
+            //}
+
+            //foreach(Dependency d in _dependencyList)
+            //{
+            //    string path = K.KrystalsFolder + @"\" + d.Name;
+            //    if(K.IsExpansionKrystalFilename(d.Name))
+            //    {
+            //        ExpansionKrystal xk = new ExpansionKrystal(path, true);
+            //    }
+            //    if(K.IsModulationKrystalFilename(d.Name))
+            //    {
+            //        ModulationKrystal mk = new ModulationKrystal(path, true);
+            //    }
+            //    if(K.IsPermutationKrystalFilename(d.Name))
+            //    {
+            //        PermutationKrystal pk = new PermutationKrystal(path, true);
+            //    }
+            //    if(K.IsPathKrystalFilename(d.Name))
+            //    {
+            //        PathKrystal pk = new PathKrystal(path, true);
+            //    }
+            //}
         }
         public List<Dependency> DependencyList { get { return _dependencyList; } }
         private List<Dependency> _dependencyList = new List<Dependency>();
-        private List<Dependency> _unknownParentsList = new List<Dependency>();
 
         private Color UsedInScoreColor = Color.Red;
-        private Color AncestorColor = Color.FromArgb(255, 0, 170, 0); 
+        private Color AncestorColor = Color.FromArgb(255, 0, 170, 0);
+        /// <summary>
+        /// Contains only name/color pairs for names of krystals used in scores and their ancestors (krystals and fields).
+        /// The default Color for other node names will be Color.Black.
+        /// </summary>
         public Dictionary<string, Color> NameColors = new Dictionary<string, Color>();
     }
 }
